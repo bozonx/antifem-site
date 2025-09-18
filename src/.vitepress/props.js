9b4c6b99df304e9de0be2a55f0fff46c32a5f788
Ivan <ipkozyrin@gmail.com>
2025-09-18 19:23:17 -0300

    add

--- src/.vitepress/props.js
+++ src/.vitepress/props.js
@@ -2,5 +2,4 @@ export const PROPS = {
   repo: "https://github.com/bozonx/antifem-site",
   hostname: "https://antifem.org",
   blogUrl: "https://blog.antifem.org",
-  footerCopyright: "Copyright © 2024-present Antifem movement.",
 };
