fa8ab017f22a3e93df772d7153527e76f5d6ba77
Ivan <ipkozyrin@gmail.com>
2025-10-02 13:50:17 -0300

    add

--- src/.vitepress/theme/index.js
+++ src/.vitepress/theme/index.js
@@ -1,5 +1,16 @@
 import DefaultTheme from "vitepress/theme";
 import "vitepress-sls-blog-tmpl/site-tmpl-fix.css";
+import { initLightbox } from "vitepress-sls-blog-tmpl/lightbox.js";
+import { resolveTranslationsByFilePath } from "vitepress-sls-blog-tmpl/resolveTranslations.js";
 import "./styles.css";
 
-export default DefaultTheme;
+/** @type {import('vitepress').Theme} */
+export default {
+  extends: DefaultTheme,
+  enhanceApp(ctx) {
+    ctx.app.config.globalProperties.getLocales = () =>
+      resolveTranslationsByFilePath(ctx.router.route.path);
+
+    initLightbox(ctx);
+  },
+};
